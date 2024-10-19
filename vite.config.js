242640f83fc12b064f029777a2326306e01a379b
kwossi <krissi.stoecker@gmail.com>
2024-10-19 14:44:43 +0200

    build

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,24 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    lib: {
+      entry: "./src/index.js", // Entry point for your library
+      name: "MyComponentLibrary", // Global name for UMD build
+      formats: ["es", "cjs"], // Specify which formats to generate
+      fileName: (format) => `my-component-library.${format}.js`, // Output file names
+    },
+    rollupOptions: {
+      // Ensure external dependencies are not bundled into your library
+      external: ["react", "react-dom"],
+      output: {
+        globals: {
+          react: "React",
+          "react-dom": "ReactDOM",
+        },
+      },
+    },
+  },
   plugins: [react()],
-})
+});
