09afe13f061472c930a442b3489f2ce5aba9ceb8
kwossi <krissi.stoecker@gmail.com>
2024-10-28 12:27:50 +0100

    build

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,8 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
 
-export default defineConfig(({ command }) => {
-  if (command === "build-library") {
+export default defineConfig(({ mode }) => {
+  if (mode === "build-library") {
     return {
       css: {
         preprocessorOptions: {
